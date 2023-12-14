e1a3cadc8a9690a447d2c05000a7b67a626eca19
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-14 19:34:01 -0300

    initializate vars & construct game skeleton with arrays

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ let ctx = canvas.getContext('2d');
 ctx.canvas.width = 0;
 ctx.canvas.height = 0;
 
+const bodyParts = [
+  [4,2,1,1],
+  [4,3,1,2],
+  [3,5,1,1],
+  [5,5,1,1],
+  [3,3,1,1],
+  [5,3,1,1]
+];
 
+let selectedWord;
+let usedLetters;
+let mistakes;
+let hits;
 
+const startGame = () => {
+  usedLetters = [];
+  mistakes = [];
+  hits = [];
+  wordContainer.innerHTML = '';
+  usedLettersElement.innerHTML = '';
+  startButton.style.display = 'none';
+}
+
+startButton.addEventListener('click', startGame);
