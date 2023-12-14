98cc7d7ba8055e1567f964c6c0afe94c91f14ba6
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-14 18:17:59 -0300

    add words of the game

---
+++ words.js
@@ -0,0 +1,12 @@
+const words = [
+  'Carne', 'Martillo', 'Lavadora', 'Sucio', 'Cangrejo', 'Lento', 'Alimentos', 'Delgado', 'Cubo',
+  'Comida', 'Caracol', 'Abajo', 'Alumno', 'Bonito', 'Cesta', 'Sol', 'Beber', 'Botella', 'Hamburguesa',
+  'Invierno', 'Cuchillo', 'Fuego', 'Llave', 'Mundo', 'Nube', 'Ojo', 'Pescado', 'Queso', 'Reloj',
+  'Silla', 'Tierra', 'Universo', 'Vaso', 'Xilófono', 'Yogur', 'Zanahoria', 'Jirafa', 'Elefante',
+  'Perro', 'Gato', 'Ratón', 'León', 'Tigre', 'Rana', 'Elefante', 'Guitarra', 'Pelota', 'Naranja',
+  'Manzana', 'Platano', 'Cielo', 'Montaña', 'Playa', 'Estrella', 'Aire', 'Lápiz', 'Libro', 'Cama',
+  'Radio', 'Diente', 'Ventana', 'Tren', 'Piano', 'Escuela', 'Azul', 'Rojo', 'Verde', 'Amarillo',
+  'Rosa', 'Negro', 'Blanco', 'Morado', 'Naranja', 'Gris', 'Café', 'Celeste', 'Circulo', 'Cuadrado',
+  'Triángulo', 'Rectángulo', 'Rombo', 'Estrella', 'Corazón', 'Flecha', 'Rayo', 'Llave', 'Puerta',
+  'Ventana', 'Espejo', 'Almohada', 'Mesa', 'Piso', 'Cielo', 'Estrella', 'Guitarra', 'Piano', 'Flor'
+];
