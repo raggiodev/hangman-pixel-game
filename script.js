5f3aabc8dd83bff33a72178cd825c7d78090cd0f
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-14 19:25:22 -0300

    create canvas

---
+++ script.js
@@ -0,0 +1,11 @@
+const usedLettersElement = document.getElementById('usedLetters');
+const wordContainer = document.getElementById('wordContainer');
+const startButton = document.getElementById('startButton');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width = 0;
+ctx.canvas.height = 0;
+
+
+
